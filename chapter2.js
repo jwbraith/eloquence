b316a9b9529e82e6eae5e5b294a950f4e5bc3e45
John Braithwaite <jwbraith@gmail.com>
2020-12-13 06:58:06 -0800

    did the chessboard exercise

--- chapter2.js
+++ chapter2.js
@@ -19,3 +19,27 @@ for (let i = 1; i <= 100; i++) {
 }
 
 //Chessboard
+let size = 8;
+let board = "";
+
+for (let j = 0; j < size; j++) {
+  if (j % 2 == 0) {
+    for (let i = 0; i < size; i++) {
+      if (i % 2 == 0) {
+        board += "#";
+      } else {
+        board += " ";
+      }
+    }
+  } else {
+    for (let i = 0; i < size; i++) {
+      if (i % 2 == 0) {
+        board += " ";
+      } else {
+        board += "#";
+      }
+    }
+  }
+  board += "\n"
+}
+console.log(board);
