30c70e867d1efc89ed2a28627dfde3c8dc4291b8
John Braithwaite <jwbraith@gmail.com>
2020-12-13 14:49:16 -0800

    did the isEven recursion exercise

--- chapter3.js
+++ chapter3.js
@@ -8,3 +8,16 @@ let minuo = (a, b) => {
 }
 console.log(minuo(22, 33));
 console.log(Math.min(22, 33));
+
+// Recursion
+let isEven = (n) => {
+  if (n == 0) {
+    return true;
+  } else if (n == 1) {
+    return false;
+  } else {
+    return isEven(n - 2);
+  }
+}
+console.log(isEven(50));
+console.log(isEven(65));
