992a5908bed60a7c9a7be8f8b198804e7d832dd0
John Braithwaite <jwbraith@gmail.com>
2020-12-15 04:29:16 -0800

    gotta split ex into more files

---
+++ chapter4.deepCompare.js
@@ -0,0 +1,21 @@
+let deepEqual = (comparer, comparand) => {
+  if (comparer === comparand) {
+    return true;
+  }
+  if (comparer == null || typeof comparer != "object" || comparand == null || typeof comparand != "object") {
+    return false;
+  }
+  let keysA = Object.keys(comparer), keysB = Object.keys(comparand);
+  if (keysA.length != keysB.length) {
+    return false;
+  }
+  for (let key of keysA) {
+    if (!keysB.includes(key) || !deepEqual(comparer[key], comparand[key])) {
+      return false;
+    }
+  }
+  return true;
+}
+let scar = { face: "slash" };
+let chin = { face: "butt" };
+console.log(deepEqual(scar, chin));
