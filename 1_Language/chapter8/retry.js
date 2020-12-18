fd62b56dfce4b233da791e975186a032d392ea8a
John Braithwaite <jwbraith@gmail.com>
2020-12-17 16:11:59 -0800

    organized into part folders

---
+++ 1_Language/chapter8/retry.js
@@ -0,0 +1,23 @@
+class MultiplicatorUnitFailure extends Error { };
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.2) {
+    return a * b;
+  } else {
+    throw new MultiplicatorUnitFailure('Klunk');
+  }
+}
+
+function reliableMultiply(a, b) {
+  for (; ;) {
+    try {
+      console.log("tried");
+      return primitiveMultiply(a, b);
+    } catch (error) {
+      if (!(error instanceof MultiplicatorUnitFailure)) {
+        throw error;
+      }
+    }
+  }
+}
+console.log(reliableMultiply(7, 7));
---
+++ 1_Language/chapter9/regular-expressions.js
@@ -0,0 +1,5 @@
+let re1 = new RegExp("abc");
+let re2 = /abc/;
+
+let blather = "The deep shame of the abcedenarian";
+console.log(blather.match(re2));
