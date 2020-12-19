ad6c2947ce1d9db9a12a399466a5f1b7efa70b92
John Braithwaite <jwbraith@gmail.com>
2020-12-19 01:26:18 -0800

    programmatic HTML table populating

--- 2_Browser/chapter13/index.js
+++ 2_Browser/chapter13/index.js
@@ -70,3 +70,23 @@ function buildTable(data) {
 }
 document.querySelector("#mountains").appendChild(buildTable(mountains));
 
+function byTagName(node, tagName) {
+  let found = [];
+  tagName = tagName.toUpperCase();
+
+  function explore(node) {
+    for (let i = 0; i < node.childNodes.length; i++) {
+      let child = node.childNodes[i];
+      if (child.nodeType == document.ELEMENT_NODE) {
+        if (child.nodeName == tagName) {
+          found.push(child);
+        } else {
+          explore(child);
+        }
+      }
+    }
+  }
+  explore(node);
+  return found;
+}
+console.log(byTagName(document.body, "h1").length);
